eccada43b69698d8fba19b71ec2a817dd11a64ba
darmaadigunaalfat <107837491+darmaadigunaalfat@users.noreply.github.com>
2022-10-15 09:02:37 +0700

    Delete main.m

--- main.m
+++
@@ -1,9 +0,0 @@
-% Power System State Estimation with PMUs (Phasor Measurement Units)..
-% Praviraj PG, Oct-2009, IIT Roorkee
-% This is the main program..
-
-num = 14;                   % Bus System..IEEE-30 or IEEE-14..
-E1 = nrlfppg(num);          % Obtain State from NRLF..
-[E2, W1] = wls(num);        % Obtain State from WLS..
-E3 = pmu(num,E2,W1);        % Estimated State from WLS+PMU..
-errors(E1,E2,E3);           % Calculate Error w.r.t NRLF and Plot Errors..
